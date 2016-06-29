524072eb33b7927948830c3ec5b5a593b2c2a18a
talelburg <talelburg11@gmail.com>
2016-06-30 01:25:37 +0300

    Made typedef Method prettier

--- SPConfig.c
+++ SPConfig.c
@@ -7,13 +7,11 @@
 #define ERR_MSG_INVALID_LINE "Invalid configuration line"
 #define ERR_MSG_VALUE_CONSTRAINT "Invalid value - constraint not met"
 #define ERR_MSG_PARAMETER(PARAM_NAME) "Parameter PARAM_NAME is not set"
-enum method
-{
+typedef enum method {
 	RANDOM,
 	MAX_SPREAD,
 	INCREMENTAL
-};
-typedef enum method Method;
+} Method;
 
 struct sp_config_t 
 {
